32ef2a18f106b434c7fc822cd82c4c5118655f8c
saeed <s.balkhair@hotmail.com>
2020-12-09 10:35:05 +0300

    Initial check-in of module lesson20

---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
+using System.Text;
+
+namespace lesson20
+{
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Student obj = new Student();
+
+            while (true)
+            {
+                Console.WriteLine("1.Add student");
+                Console.WriteLine("2.show all students");
+                Console.WriteLine("3.show GPA students");
+                Console.WriteLine("4.show Max GPA student");
+                Console.WriteLine("5.show Min GPA student");
+                Console.WriteLine("6.delete student");
+                Console.WriteLine("\n");
+                Console.WriteLine("Select number from 1 to 6:");
+                var input = Console.ReadLine();
+
+
+                switch (input)
+                {
+                    case "1":
+                        Console.WriteLine("Enter your name");
+                        var name = Console.ReadLine();
+                        Console.WriteLine("Enter course name");
+                        var course = Console.ReadLine();
+                        Console.WriteLine("Enter grade 1");
+                        var grade1 = Console.ReadLine();
+                        Console.WriteLine("Enter grade 2");
+                        var grade2 = Console.ReadLine();
+                        Console.WriteLine("Enter grade 3");
+                        var grade3 = Console.ReadLine();
+
+                        List<int> grades = new List<int>();
+                        grades.Add(Convert.ToInt32(grade1));
+                        grades.Add(Convert.ToInt32(grade2));
+                        grades.Add(Convert.ToInt32(grade3));
+
+
+                        var student = new Student();
+                        student.Name = name;
+                        student.Course = course;
+                        student.Grades = grades;
+                        obj.AddStudent(student);
+                        break;
+                    case "2":
+                        obj.GetAllStudent();
+                        obj.MakeLine();
+                        break;
+
+                    case "3":
+                        obj.GetAllStudentGPA();
+                        obj.MakeLine();
+                        break;
+                    case "4":
+                        obj.GetMaxGPA();
+                        obj.MakeLine();
+                        break;
+                    case "5":
+                        obj.GetMinGPA();
+                        obj.MakeLine();
+                        break;
+                    case "6":
+                        Console.Write("Write Name to delete:");
+                        var username = Console.ReadLine();
+                        obj.DeleteStudent(username);
+                        obj.MakeLine();
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+
+        }
+
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Permissions;
+
+namespace lesson20
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Course { get; set; }
+        public List<int> Grades { get; set; }
+        public List<Student> students = new List<Student>();
+        public Student()
+        {
+
+
+        }
+
+        public void MakeLine()
+        {
+            Console.WriteLine("-----------------------------------");
+        }
+        public void AddStudent(Student student)
+        {
+            students.Add(student);
+            Console.WriteLine("success add user");
+        }
+
+        public void DeleteStudent(string name)
+        {
+
+            for (int i = 0; i < students.Count; i++)
+            {
+                if (students[i].Name == name)
+                {
+                    students.Remove(students[i]);
+                }
+            }
+            Console.WriteLine("success remove user");
+            this.GetAllStudent();
+
+
+        }
+
+        public void GetAllStudent()
+        {
+            foreach (var student in students)
+            {
+                Console.WriteLine($"name:{student.Name} course:{student.Course} grades[{student.Grades[0]},{student.Grades[2]},{student.Grades[2]}]");
+            }
+        }
+
+        public void GetStudent(Student student)
+        {
+            Console.Write($"name:{student.Name} course:{student.Course} grades[{student.Grades[0]},{student.Grades[2]},{student.Grades[2]}]");
+        }
+
+        public void GetMaxGPA()
+        {
+            var max = students[0];
+            foreach (var student in students)
+            {
+                if (GetGPA(student.Grades) > GetGPA(max.Grades))
+                {
+                    max = student;
+                }
+
+            }
+            Console.WriteLine($"Max Grade {max.Name}  GPA:{GetGPA(max.Grades)}");
+
+        }
+
+        private decimal GetGPA(List<int> grades)
+        {
+            var total = 0;
+            foreach (var grade in grades)
+            {
+                total += grade;
+            }
+
+            return total / 3;
+        }
+
+
+        public void GetAllStudentGPA()
+        {
+
+            foreach (var student in students)
+            {
+                Console.WriteLine($"{student.Name} {GetGPA(student.Grades)}");
+
+            }
+
+        }
+
+        public void GetMinGPA()
+        {
+            var min = students[0];
+            foreach (var student in students)
+            {
+                if (GetGPA(student.Grades) < GetGPA(min.Grades))
+                {
+                    min = student;
+                }
+
+            }
+            Console.WriteLine($"Min Grade {min.Name}  GPA:{GetGPA(min.Grades)}");
+        }
+
+
+    }
+}
